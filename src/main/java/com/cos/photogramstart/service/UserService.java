9e6bdb31dc8f6e59762a617d95dbbff530d4daee
Junmo <hongjm1022@gmail.com>
2024-07-02 14:31:23 +0900

    회원가입, 인증, 유효성검사

---
+++ src/main/java/com/cos/photogramstart/service/UserService.java
@@ -0,0 +1,39 @@
+package com.cos.photogramstart.service;
+
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.domain.user.UserRepository;
+import com.cos.photogramstart.handler.ex.CustomValidationApiException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.function.Supplier;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Transactional
+    public User 회원수정(int id,User user){
+
+        //1. 영속화
+        User userEntity = userRepository.findById(id).orElseThrow(()->{ return new CustomValidationApiException("찾을 수 없는 id입니다.");});
+
+        //2. 영속화된 오브젝트를 수정
+
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+
+        userEntity.setName(user.getName());
+        userEntity.setPassword(encPassword);
+        userEntity.setBio(user.getBio());
+        userEntity.setWebsite(user.getWebsite());
+        userEntity.setPhone(user.getPhone());
+        userEntity.setGender(user.getGender());
+        return userEntity;
+    }
+}
